a0d65a31d615a57f42c5efa595eda0e82e0f53cc
NIKDISSV-Forever <70575593+NIKDISSV-Forever@users.noreply.github.com>
2021-01-11 12:49:44 +0600

    Update brutyry.py

--- brutyry.py
+++ brutyry.py
@@ -1,24 +1,42 @@
 # -*- coding: utf-8 -*-
 from sys import stdout
 
-def generate(number_of_characters, file_name, chapel):
-	already = 0
-	file = open(file_name, 'a')
 
-	while already <= chapel:
-		result = str(already).rjust(number_of_characters, '0')
-		file.write(result)
-		stdout.write(result+'\n')
-		already += 1
+def generate(
+    number_of_characters,
+    file_name,
+    chapel,
+):
+
+    already = 0
+    file = open(file_name, 'a')
+
+    while already <= chapel:
+        result = str(already).rjust(number_of_characters, '0')
+        file.write(result)
+        stdout.write(result+'\n')
+        already += 1
+
+
 while 1:
-	try:
-		number_of_characters = int(input("Кол-во символов: "))
-		chapel = int('9' * number_of_characters)
-		break
-	except Exception as error:
-		stdout.write(str(error)+'\n')
+
+    try:
+        number_of_characters = int(input("Кол-во символов: "))
+        chapel = int('9' * number_of_characters)
+        break
+    except Exception as error:
+        stdout.write(str(error)+'\n')
 
 file_name = input("Введите имя файла (любое): ")
+
+
 if file_name.isspace() or file_name == '':
-	file_name = 'wordlist.txt'
-generate(number_of_characters, file_name, chapel)
+
+    file_name = 'wordlist.txt'
+
+
+generate(
+    number_of_characters,
+    file_name,
+    chapel,
+)
