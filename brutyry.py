ede5069d3966c458c5025c6c99e7672bc37c6adb
Dima Denissov <nikdossv.github.forever@gmail.com>
2021-02-10 20:56:46 +0600

    Redesigned from scratch.

--- brutyry.py
+++ brutyry.py
@@ -1,48 +1,91 @@
-# -*- coding: utf-8 -*-
-from sys import stdout, argv
-
-number_of_characters, file_name = None, None
-if "-nc" in argv:
-    if argv.index("-nc") != argv[-1]:
-        tmp = argv[argv.index("-nc")+1]
-        number_of_characters = round(int(tmp)) if tmp.isnumeric() else None
-        del tmp
-if "-fn" in argv:
-    if argv.index("-fn") != argv[-1]:
-        number_of_characters = argv[argv.index("-fn")+1]
-
-def generate(
-    number_of_characters,
-    file_name,
-    chapel,
-):
-
-    file = open(file_name, 'a')
-    for i in range(chapel): file.write(str(i).rjust(number_of_characters, '0')+'\n')
-    
-    file.close()
+import sys
 
 
-while 1:
+class Brutyry:
 
-    try:
-        if not number_of_characters:
-            number_of_characters = int(input("??????? ???-?? ????????: "))
-        chapel = int('9' * number_of_characters) + 1
-        break
-    except Exception as error:
-        stdout.write(str(error)+'\n')
+    def __init__(self, argv):
 
-if not file_name:
-    file_name = input("??? ????? (wordlist.txt): ")
+        self.file_name = "wordlist.txt"
+        if "-fn" in argv:
+            try:
+                self.file_name = argv[
+                    argv.index("-fn")+1
+                ]
+            except:
+                pass
+
+        self.len = 4
+        if "-len" in argv:
+            try:
+                self.len = int(
+                    argv[
+                        argv.index("-len")+1
+                    ]
+                )
+            except:
+                pass
+
+        self.append = False
+        if "-A" in argv:
+            self.append = True
+
+        self.out = False
+        if "-out" in argv:
+            self.out = True
+
+        self.timer = False
+        if "-T" in argv:
+            self.timer = True
+
+        if self.out:
+            sys.stdout.write(
+                f"File: {self.file_name}\nLen of pass: {self.len}\nStdout: {self.out}\n\nIs that correct? (If yes, press Enter) "
+            )
+            for i in sys.stdin:
+                if i.strip() == "":
+                    break
+                else:
+                    sys.stdout.write(
+                        "\nRestart the program with the correct parameters.\n\n"
+                    )
+                    quit(1)
+
+        self.max_ = map(
+            lambda x: str(x).rjust(self.len, "0") + "\n",
+            range(
+                int("9" * self.len) + 1)
+        )
+
+        self.generate()
+
+    def generate(self):
+
+        if self.timer:
+            from time import time
+            TIMED = time()
+        with open(self.file_name, ("a" if self.append else "w")) as f:
+            args = tuple([f])
+            if self.out:
+                self.generate_Out(*args)
+            else:
+                self.generate_noOut(*args)
+        if self.timer:
+            self.TIMED = time() - TIMED
+            sys.stdout.write(str(self.TIMED))
+
+    def generate_noOut(self, fileH):
+        for l in self.max_:
+            fileH.write(l)
+
+    def generate_Out(self, fileH):
+        for l in self.max_:
+            fileH.write(l)
+            sys.stdout.write(l)
+
+    def generater(self):
+        for l in self.max_:
+            yield l
 
 
-if (file_name.isspace() or file_name == ''):
-    file_name = 'wordlist.txt'
-
-
-generate(
-    number_of_characters,
-    file_name,
-    chapel,
-)
+if __name__ == "__main__":
+    Brutyry(sys.argv)
